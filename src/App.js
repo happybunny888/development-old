594c5702fe356522087f1f404d6fedef63d45bc4
Rachel Ma <rara80000@gmail.com>
2022-11-18 17:25:45 -0500

    whee

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+//import { useState } from "react";
 import TicketData from "./assets/ticket-data.json";
-import TicketItem from "./components/TicketItem.js";
+//import TicketItem from "./components/TicketItem.js";
 import Aggregator from "./components/Aggregator.js";
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 TicketData.forEach((item) => {
@@ -12,51 +12,7 @@ TicketData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-  const [cart, setCart] = useState([]);
-  //const [count, setCount] = useState([])
-  function addToCart(e, item) {
-    alert(" You clicked add " + item.name + "  price is " + item.price);
-    //setCount(cart.append(name, price));
-    //if (cart.indexOf(currentCart) !== -1) return;
-    e.preventDefault();
-    //if (cart.indexOf(currentCart) !== -1) return;
-
-    setCart((cart) => {
-      const existingIdx = cart.findIndex((i) => i.name === item.name);
-      if (existingIdx !== -1) {
-        return [
-          ...cart.slice(0, existingIdx),
-          { ...cart[existingIdx], count: cart[existingIdx].count + 1 },
-          ...cart.slice(existingIdx + 1),
-        ];
-      }
-      return [...cart, { ...item, count: 1 }];
-    });
-    //console.log(cart);
-  }
-  function removeFromCart(e, item) {
-    alert(" You clicked remove " + item.name + " , price is " + item.price);
-    e.preventDefault();
-
-    setCart((cart) => {
-      const existingIdx = cart.findIndex((i) => i.name === item.name);
-      
-      if (existingIdx !== -1 && cart[existingIdx].count >1 ){ /*if item exists in cart*/ 
-        return [
-          ...cart.slice(0, existingIdx),
-          { ...cart[existingIdx], count: cart[existingIdx].count - 1 },
-          ...cart.slice(existingIdx + 1),
-        ];
-      }
-      if (existingIdx !== -1 && cart[existingIdx].count >0 ){ /*if item exists in cart*/ 
-        return [
-          ...cart.slice(0, existingIdx), ...cart.slice(existingIdx + 1),
-        ];
-      } 
-      return [...cart];
-    });
-    //console.log(cart);
-  }
+  
 
   return (
     <div className="App">
